863af153c8e85d4852f29d58c0097d8b22ccf9e7
Geoffroy Streit <geoffroy.streit@gmail.com>
2026-02-17 23:14:50 +0100

    Fix COOP/COEP headers to enable OPFS for SQLite

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,9 @@ export default defineConfig({
         headers: {
             'Cross-Origin-Opener-Policy': 'same-origin',
             'Cross-Origin-Embedder-Policy': 'require-corp'
+        },
+        fs: {
+            strict: false
         }
     }
 });
